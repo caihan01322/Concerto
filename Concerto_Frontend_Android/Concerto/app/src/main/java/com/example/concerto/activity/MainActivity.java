9689b6239f87b43ebe0dd2e7786116ec901ef48d
caihan <2249618917@qq.com>
2021-04-27 17:23:45 +0800

    commit XML调整

--- Concerto_Frontend_Android/Concerto/app/src/main/java/com/example/concerto/activity/MainActivity.java
+++ Concerto_Frontend_Android/Concerto/app/src/main/java/com/example/concerto/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.concerto;
+package com.example.concerto.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.concerto.R;
 
 public class MainActivity extends AppCompatActivity {
 
