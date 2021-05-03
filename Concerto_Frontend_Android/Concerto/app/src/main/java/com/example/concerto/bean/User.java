e6a0545392cd8203ad3b5372aad448811c968943
caihan <2249618917@qq.com>
2021-05-03 19:24:23 +0800

    创建部分bean

---
+++ Concerto_Frontend_Android/Concerto/app/src/main/java/com/example/concerto/bean/User.java
@@ -0,0 +1,9 @@
+package com.example.concerto.bean;
+
+public class User {
+    public Long userId;
+    public String userPhone;
+    public String userName;
+    public String userEmail;
+    public String userIntroducton;
+}
